becc1de588c467ac330bbba1628f9c95fdcae997
Curtis Lassam <curtis@lassam.net>
2017-11-13 20:03:09 -0800

    tidy up round here, we have company coming

--- conway.py
+++ conway.py
@@ -23,10 +23,6 @@ def set(array, val):
         array[x][y] = val
 
 
-@cuda.jit(device=True)
-def coinToss(xoro_random_states, x, y):
-    return xoroshiro128p_uniform_float32(xoro_random_states, x*y) > 0.5
-
 @cuda.jit
 def setRandom(array, xoro_random_states):
     x, y = cuda.grid(2)
@@ -38,34 +34,40 @@ def setRandom(array, xoro_random_states):
 def conway(array, newArray):
     x, y = cuda.grid(2)
     if x < array.shape[0] and y < array.shape[1]:
+
         neighbors = 0
+        if (x+1) < array.shape[0] and array[x+1][y]:
+            neighbors += 1
+        if (y+1) < array.shape[1] and array[x][y+1]:
+            neighbors += 1
+        if (x-1) >= 0 and array[x-1][y]:
+            neighbors += 1
+        if (y-1) >= 0 and array[x][y-1]:
+            neighbors += 1
+
+        def dead():
+            newArray[x][y] = False
+        def alive():
+            newArray[x][y] = True
+
         live = array[x][y]
-
-        if (x+1) < array.shape[0]:
-            if array[x+1][y]:
-                neighbors += 1
-        if (y+1) < array.shape[1]:
-            if array[x][y+1]:
-                neighbors += 1
-        if (x-1) >= 0:
-            if array[x-1][y]:
-                neighbors += 1
-        if (y-1) >= 0:
-            if array[x][y-1]:
-                neighbors += 1
-
         if live:
+            # Any live cell with fewer than two live neighbors dies, as if caused by underpopulation
             if neighbors < 2:
-                newArray[x][y] = False
+                dead()
+            # Any live cell with two or three live neighbors lives on to the next generation
             if neighbors == 2 or neighbors == 3:
-                newArray[x][y] = True
+                alive()
+            # Any live cell with more than three live neighbors dies, as if by overpopulation
             if neighbors > 3:
-                newArray[x][y] = False
+                dead()
         else:
+            # Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
             if neighbors == 3:
-                newArray[x][y] = True
+                alive()
+            # Any other dead cell stays dead, Jim
             else:
-                newArray[x][y] = False
+                dead()
 
 
 class Grid:
@@ -106,9 +108,12 @@ class BinaryGrid(Grid):
 class ConwayGrid(BinaryGrid):
 
     def step(self):
+        start = timeit.default_timer()
         empty_grid = numba.cuda.device_array([self.width, self.height], dtype=bool)
         kernel(conway, self.grid)(self.grid, empty_grid)
         self.grid = empty_grid
+        stop = timeit.default_timer()
+        print("Step: ", (stop-start)*1000)
 
 
 bg = ConwayGrid(1000, 1000)
@@ -120,8 +125,6 @@ bg.random(seed=time.time())
 stop = timeit.default_timer()
 print("Randomize: ", stop-start)
 
-bg.samplePrint()
-bg.step()
-bg.samplePrint()
-bg.step()
+for i in range(0, 100):
+    bg.step()
 bg.samplePrint()
