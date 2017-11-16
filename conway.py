0a5e9f93e12d4d39d0e4d82e4a0a586e4b0f0e8e
Curtis Lassam <curtis@lassam.net>
2017-11-15 19:24:19 -0800

    split the code into grid for core stuff and conway for conway specific stuff

--- conway.py
+++ conway.py
@@ -1,34 +1,8 @@
-import math
 import timeit
-import numba
 import time
 from numba import cuda
-from numba.cuda.random import create_xoroshiro128p_states, xoroshiro128p_uniform_float32
 
-from functools import partial
-
-
-def kernel(operation, array):
-    threads_per_block = (16, 16)
-    blocks_per_grid_x = math.ceil(array.shape[0] / threads_per_block[0])
-    blocks_per_grid_y = math.ceil(array.shape[1] / threads_per_block[1])
-    blocks_per_grid = (blocks_per_grid_x, blocks_per_grid_y)
-    return operation[blocks_per_grid, threads_per_block]
-
-
-@cuda.jit
-def set(array, val):
-    x, y = cuda.grid(2)
-    if x < array.shape[0] and y < array.shape[1]:
-        array[x][y] = val
-
-
-@cuda.jit
-def setRandom(array, xoro_random_states):
-    x, y = cuda.grid(2)
-    if x < array.shape[0] and y < array.shape[1]:
-        array[x][y] = (xoroshiro128p_uniform_float32(xoro_random_states, x) * y % 1) > 0.5
-
+from grid import BinaryGrid, kernel
 
 @cuda.jit
 def conway(array, newArray):
@@ -70,53 +44,22 @@ def conway(array, newArray):
                 dead()
 
 
-class Grid:
-    def get(self, x, y):
-        return self.grid[x][y]
-
-    def slice(self, x1, y1, x2, y2):
-        # TODO: this
-        pass
-
-
-class BinaryGrid(Grid):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.grid = numba.cuda.device_array([width, height], dtype=bool)
-
-    def clear(self):
-        kernel(set, self.grid)(self.grid, False)
-
-    def mask(self):
-        kernel(set, self.grid)(self.grid, True)
-
-    def random(self, seed=1):
-        rng_states = create_xoroshiro128p_states(self.width, seed=seed)
-        kernel(setRandom, self.grid)(self.grid, rng_states)
-
-    def samplePrint(self):
-        for i in range(0, 40):
-            for j in range(0, 40):
-                if self.get(i, j):
-                    print(".", end="")
-                else: print("X", end="")
-            print("")
-        print("-"*40)
-
-
 class ConwayGrid(BinaryGrid):
 
     def step(self):
         start = timeit.default_timer()
-        empty_grid = numba.cuda.device_array([self.width, self.height], dtype=bool)
+        empty_grid = cuda.device_array([self.width, self.height], dtype=bool)
         kernel(conway, self.grid)(self.grid, empty_grid)
         self.grid = empty_grid
         stop = timeit.default_timer()
         print("Step: ", (stop-start)*1000)
 
 
+
+start = timeit.default_timer()
 bg = ConwayGrid(1000, 1000)
+stop = timeit.default_timer()
+print("Instantiate: ", stop-start)
 
 print(bg.get(5, 5))
 
--- grid.py
+++ grid.py
@@ -1,43 +1,15 @@
 import math
 import timeit
-import numba
+
 from numba import cuda
-from functools import partial
+from numba.cuda.random import create_xoroshiro128p_states, xoroshiro128p_uniform_float32
 
-
-def cudaMap(array, operation):
+def kernel(operation, array):
     threads_per_block = (16, 16)
-    blocks_per_grid_x = math.ceil(cuda_array.shape[0] / threads_per_block[0])
-    blocks_per_grid_y = math.ceil(cuda_array.shape[1] / threads_per_block[1])
+    blocks_per_grid_x = math.ceil(array.shape[0] / threads_per_block[0])
+    blocks_per_grid_y = math.ceil(array.shape[1] / threads_per_block[1])
     blocks_per_grid = (blocks_per_grid_x, blocks_per_grid_y)
-    operation[blocks_per_grid, threads_per_block](array)
-
-
-def cudaApply(array, otherArray, operation):
-    threads_per_block = (16, 16)
-    blocks_per_grid_x = math.ceil(cuda_array.shape[0] / threads_per_block[0])
-    blocks_per_grid_y = math.ceil(cuda_array.shape[1] / threads_per_block[1])
-    blocks_per_grid = (blocks_per_grid_x, blocks_per_grid_y)
-    operation[blocks_per_grid, threads_per_block](array, otherArray)
-
-
-@cuda.jit(device=True)
-def incr(array, x, y):
-    return array[x][y] + 1
-
-
-@cuda.jit
-def increment_by_one(array):
-    x, y = cuda.grid(2)
-    if x < array.shape[0] and y < array.shape[1]:
-        array[x][y] = incr(array, x, y)
-
-
-@cuda.jit
-def add_two_arrays(array, otherArray):
-    x, y = cuda.grid(2)
-    if x < array.shape[0] and y < array.shape[1]:
-        array[x][y] = array[x][y] + otherArray[x][y]
+    return operation[blocks_per_grid, threads_per_block]
 
 
 @cuda.jit
@@ -47,36 +19,44 @@ def set(array, val):
         array[x][y] = val
 
 
-class BinaryGrid:
-    def __init__(self, x, y):
-        self.grid = numba.cuda.device_array([x, y], dtype=bool)
+@cuda.jit
+def setRandom(array, xoro_random_states):
+    x, y = cuda.grid(2)
+    if x < array.shape[0] and y < array.shape[1]:
+        array[x][y] = (xoroshiro128p_uniform_float32(xoro_random_states, x) * y % 1) > 0.5
+
+
+class Grid:
+    def get(self, x, y):
+        return self.grid[x][y]
+
+    def slice(self, x1, y1, x2, y2):
+        # TODO: this
+        pass
+
+
+class BinaryGrid(Grid):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = cuda.device_array([width, height], dtype=bool)
 
     def clear(self):
-        cudaMap(self.grid, partial(val=0))
+        kernel(set, self.grid)(self.grid, False)
 
+    def mask(self):
+        kernel(set, self.grid)(self.grid, True)
 
-# instantiate
-start = timeit.default_timer()
-cuda_array = numba.cuda.device_array([10000, 10000], dtype=int)
-cuda_array_2 = numba.cuda.device_array([10000, 10000], dtype=int)
-stop = timeit.default_timer()
-print("Instantiation: ", stop-start)
+    def random(self, seed=1):
+        rng_states = create_xoroshiro128p_states(self.width, seed=seed)
+        kernel(setRandom, self.grid)(self.grid, rng_states)
 
-# increment
-start = timeit.default_timer()
-cudaMap(cuda_array, increment_by_one)
-cudaMap(cuda_array, increment_by_one)
-cudaMap(cuda_array_2, increment_by_one)
-cudaApply(cuda_array, cuda_array_2, add_two_arrays)
-stop = timeit.default_timer()
-print("Increment: ", stop-start)
+    def samplePrint(self):
+        for i in range(0, 40):
+            for j in range(0, 40):
+                if self.get(i, j):
+                    print(".", end="")
+                else: print("X", end="")
+            print("")
+        print("-"*40)
 
-# print(giant_array[99999])
-
-# access
-start = timeit.default_timer()
-print("Array 1:", cuda_array[100][100])
-print("Array 2:", cuda_array_2[100][100])
-stop = timeit.default_timer()
-
-print("Access: ", stop-start)
